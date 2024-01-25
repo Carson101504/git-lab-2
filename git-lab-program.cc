a88154e8c9242f07e5ee10248cdf5c27a4d1ef34
Carson Mackey <cm515122@ohio.edu>
2024-01-25 16:08:17 -0500

    Committing git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Carson Mackey
+ *        Date: 24 January 2024
+ * Description: git clone repository demo
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
